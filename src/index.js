e3a4a6db2d5439b7ab13726ad89de49f1cc97783
Udaymetta <mudayk99@gmail.com>
2023-12-10 23:45:45 +0530

    comments dev

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Top from "./top";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
   <React.StrictMode>
     <App />
+    <Top />
   </React.StrictMode>
 )
 
---
+++ src/top.js
@@ -0,0 +1,10 @@
+const Top = () => {
+    return (
+        <div className="top-card">
+            
+            <a href="#" className="top-nav"> &uarr; TOP</a>
+        </div>
+    )
+}
+
+export default Top;
